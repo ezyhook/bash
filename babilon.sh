c6e84c26e22cea476ef7beaa80696ee013a7b2f8
ezyhook <113023731+ezyhook@users.noreply.github.com>
2024-03-07 13:10:31 +0300

    Update babilon.sh

--- babilon.sh
+++ babilon.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-
+babway="$HOME/go/bin"
 invento="$HOME/faucet/babnodes.txt"
 while read line 
     do
@@ -7,5 +7,14 @@ while read line
         TOKEN="$(echo "$line" | cut -d ' ' -f2)"
         wallet="$(echo "$line" | cut -d ' ' -f3)"
         curl  https://discord.com/api/v10/channels/1075371070493831259/messages -X POST -H "Content-Type: application/json" -H "Authorization: $TOKEN" -d '{"content": "!faucet '"$wallet"'"}'
+        sleep 1
+    done < "$invento"
+
+sleep 10000
+
+while read line1
+    do
+        wallet1="$(echo "$line1" | cut -d ' ' -f3)"
+        "$babway"/babylond tx bank send "$wallet1" "$1" 99990ubbn --fee-payer "$wallet1" --node https://rpc.testnet3.babylonchain.io:443 --fees 10ubbn -y
         sleep 1
     done < "$invento"
