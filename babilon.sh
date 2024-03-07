e18cf9a3e86fbc59fc6c9524408e6b86455d6be8
ezyhook <113023731+ezyhook@users.noreply.github.com>
2024-03-07 11:07:51 +0300

    Update babilon.sh

--- babilon.sh
+++ babilon.sh
@@ -1,13 +1,11 @@
 #/bin/bash
 
 invento="$HOME/faucet/babnodes.txt"
-IFS=' '
 while read line 
     do
-    name="$(echo "$line" | cut -d ' ' -f1)"
-    TOKEN="$(echo "$line" | cut -d ' ' -f2)"
-    wallet="$(echo "$line" | cut -d ' ' -f3)"
-    #echo "$name - $TOKEN - $wallet"
-    curl  https://discord.com/api/v10/channels/1075371070493831259/messages -X POST -H "Content-Type: application/json" -H "Authorization: $TOKEN" -d '{"content": "!faucet '"$wallet"'"}'
-    sleep 1
+        name="$(echo "$line" | cut -d ' ' -f1)"
+        TOKEN="$(echo "$line" | cut -d ' ' -f2)"
+        wallet="$(echo "$line" | cut -d ' ' -f3)"
+        curl  https://discord.com/api/v10/channels/1075371070493831259/messages -X POST -H "Content-Type: application/json" -H "Authorization: $TOKEN" -d '{"content": "!faucet '"$wallet"'"}'
+        sleep 1
     done < "$invento"
