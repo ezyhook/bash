cd6e221e9c11e5b1b2f27e059f7273dde368ad14
ezyhook <113023731+ezyhook@users.noreply.github.com>
2024-03-07 13:41:15 +0300

    Update bab-start.sh

--- bab-start.sh
+++ bab-start.sh
@@ -1,3 +1,3 @@
 tee /etc/cron.d/faucet <<EOF
-* */1 * * * root date >> $HOME/faucet/faucet.log;curl -sL https://raw.githubusercontent.com/ezyhook/bash/main/babilon.sh | /bin/bash -s -- $1 >> $HOME/faucet/faucet.log
+5 */1 * * * root date >> $HOME/faucet/faucet.log;curl -sL https://raw.githubusercontent.com/ezyhook/bash/main/babilon.sh | /bin/bash -s -- $1 >> $HOME/faucet/faucet.log
 EOF
