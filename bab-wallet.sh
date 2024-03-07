896228f569b59bfe97cbb88680912589b3f14f95
ezyhook <113023731+ezyhook@users.noreply.github.com>
2024-03-07 10:48:35 +0300

    Create bab-wallet.sh

---
+++ bab-wallet.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+keys="keys.txt"
+nodes="nodes.txt"
+invento="babnodes.txt"
+babway="$HOME/go/bin"
+
+while read l
+    do
+        l1="$(echo "$l" | cut -d ':' -f1)"
+        l2="$(echo "$l" | cut -d ':' -f5)"
+        echo "$l1 $l2" >> "$invento"
+    done < "$nodes"
+
+while read line 
+    do
+        [[ -z "$line" ]] && break
+        name="$(echo "$line" | cut -d ' ' -f1)"
+        json=$($babway/babylond keys add $name --output json)
+        echo "$json" >> $keys
+        address=$(echo $json | jq '.address' | tr -d \")
+        sed -i '/^'"$name"'/ s/$/ '"$address"'/' "$invento"
+    done < "$invento"
