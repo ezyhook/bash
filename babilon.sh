357f5145d143476fc314e7a84eeb39dda706f1d8
ezyhook <113023731+ezyhook@users.noreply.github.com>
2024-03-09 21:06:30 +0300

    Update babilon.sh

--- babilon.sh
+++ babilon.sh
@@ -8,10 +8,10 @@ while read line
         TOKEN="$(echo "$line" | cut -d ' ' -f2)"
         wallet="$(echo "$line" | cut -d ' ' -f3)"
         curl  https://discord.com/api/v10/channels/1075371070493831259/messages -X POST -H "Content-Type: application/json" -H "Authorization: $TOKEN" -d '{"content": "!faucet '"$wallet"'"}'
-        sleep 1
+        sleep $((1 + $RANDOM % 7))
     done < "$invento"
 
-sleep $((1 + $RANDOM % 7))
+sleep 1200
 
 while read line1
     do
