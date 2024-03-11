970e6c14a7d536ef00e74a96e81766eee4b8868f
ezyhook <113023731+ezyhook@users.noreply.github.com>
2024-03-11 17:09:49 +0300

    Update bab-check.sh

--- bab-check.sh
+++ bab-check.sh
@@ -8,5 +8,5 @@ while read line1
     do
         [[ -z "$line1" ]] && break
         wallet1="$(echo "$line1" | cut -d ' ' -f3)"
-        echo $("$babway"/babylond q bank balances "$wallet1" --output json --node https://rpc.testnet3.babylonchain.io:443)
+        echo $("$babway"/babylond q bank balances "$wallet1" --output json --node https://babylon-testnet-rpc.polkachu.com:443)
     done < "$invento"
