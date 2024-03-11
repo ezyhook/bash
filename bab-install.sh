297a0c57057831399093f57503894a5097ed766f
ezyhook <113023731+ezyhook@users.noreply.github.com>
2024-03-11 09:26:23 +0300

    Update bab-install.sh

--- bab-install.sh
+++ bab-install.sh
@@ -1,12 +1,11 @@
+cd "$HOME"
 wget https://go.dev/dl/go1.22.1.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.1.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 echo "export PATH=$PATH:/usr/local/go/bin" >> $HOME/.bashrc
 echo "export PATH=$PATH:/usr/local/go/bin" >> $HOME/.bash_profile
-cd "$HOME"
 git clone https://github.com/babylonchain/babylon.git
 cd babylon
-git checkout v0.8.3
 make install
 ~/go/bin/babylond init NODENAME --chain-id bbn-test-3
 ~/go/bin/babylond config set client chain-id bbn-test-3
