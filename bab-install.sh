d78220b929216cb46ee846ebd8d1c73e59ba13ce
ezyhook <113023731+ezyhook@users.noreply.github.com>
2024-03-11 09:42:56 +0300

    Update bab-install.sh

--- bab-install.sh
+++ bab-install.sh
@@ -1,10 +1,10 @@
-sudo apt -qy install git build-essential curl jq lz4 screen
 cd "$HOME"
-wget https://go.dev/dl/go1.22.1.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.1.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
-echo "export PATH=$PATH:/usr/local/go/bin" >> $HOME/.bashrc
-echo "export PATH=$PATH:/usr/local/go/bin" >> $HOME/.bash_profile
+(command -v go &> /dev/null) || { sudo apt -qy install git build-essential curl jq lz4 screen;
+wget https://go.dev/dl/go1.22.1.linux-amd64.tar.gz; 
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.1.linux-amd64.tar.gz;
+export PATH=$PATH:/usr/local/go/bin;
+echo "export PATH=$PATH:/usr/local/go/bin" >> $HOME/.bashrc;
+echo "export PATH=$PATH:/usr/local/go/bin" >> $HOME/.bash_profile; }
 git clone https://github.com/babylonchain/babylon.git
 cd babylon
 . $HOME/.bash_profile
