dd147e7b2724dba275345d350d285cdea6009369
ezyhook <113023731+ezyhook@users.noreply.github.com>
2024-03-12 15:19:46 +0300

    Create babilon2.sh

---
+++ babilon2.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+babway="$HOME/go/bin"
+invento="$HOME/faucet/babnodes.txt"
+
+test=$(~/go/bin/babylond q bank balances bbn15r3s8knp272jzfq004e0hyae0qj8p595x0ml8q --output json --node https://babylon-testnet-rpc.polkachu.com:443 | jq .balances[])
+if [[ -n "$test" ]]
+then
+    bal=$(echo "$test" | jq .amount | tr -d \");
+    if [[ "$bal" -gt 2 ]]
+    then
+        while read line
+        do
+            [[ -z "$line" ]] && break
+            name="$(echo "$line" | cut -d ' ' -f1)"
+            TOKEN="$(echo "$line" | cut -d ' ' -f2)"
+            wallet="$(echo "$line" | cut -d ' ' -f3)"
+            date --utc -d "+3 hours"
+            curl  https://discord.com/api/v10/channels/1075371070493831259/messages -X POST -H "Content-Type: application/json" -H "Authorization: $TOKEN" -d '{"content": "!faucet '"$wallet"'"}'
+            sleep 1
+        done < "$invento"
+        sleep 21700
+    fi
+fi
