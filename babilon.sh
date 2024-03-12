f604ef5a83385f3cf94a5439bf09c72649a4ddde
ezyhook <113023731+ezyhook@users.noreply.github.com>
2024-03-12 10:05:53 +0300

    Update babilon.sh

--- babilon.sh
+++ babilon.sh
@@ -8,7 +8,7 @@ while read line
         name="$(echo "$line" | cut -d ' ' -f1)"
         TOKEN="$(echo "$line" | cut -d ' ' -f2)"
         wallet="$(echo "$line" | cut -d ' ' -f3)"
-        date --date='TZ="Europe/Moscow"'
+        date
         curl  https://discord.com/api/v10/channels/1075371070493831259/messages -X POST -H "Content-Type: application/json" -H "Authorization: $TOKEN" -d '{"content": "!faucet '"$wallet"'"}'
         sleep 3
     done < "$invento"
@@ -19,7 +19,7 @@ while read line1
     do
         [[ -z "$line1" ]] && break
         wallet1="$(echo "$line1" | cut -d ' ' -f3)"
-        date --date='TZ="Europe/Moscow"'
+        date
         "$babway"/babylond tx bank send "$wallet1" "$target" 99990ubbn --fee-payer "$wallet1" --node https://babylon-testnet-rpc.polkachu.com:443 --fees 10ubbn -y
         sleep 1
     done < "$invento"
